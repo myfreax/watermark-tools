368609d1b9b3492357653da669752eb4ccc80f8c
黄学坚 <313566117@qq.com>
2017-01-20 18:58:16 +0800

    添加字体配置文件

--- src/config/font.js
+++ src/config/font.js
@@ -2,4 +2,10 @@
  * Created by Freax on 17-1-18.
  * @blog http://www.myfreax.com
  */
-export default ['Open Sans','Helvetica Neue','Arial','Hiragino Sans GB','Microsoft YaHei','WenQuanYi Micro Hei','sans-serif']
+/**
+ *  {name:'字体名称',src:['加载字体路径或者url','加载字体路径或者url（可选）'.....]},
+ * @type {[*]}
+ */
+module.exports =  [
+    {name:'sourceSansPro',src:['font/sourcesanspro-regular.woff','font/sourcesanspro-regular.ttf']},
+];
