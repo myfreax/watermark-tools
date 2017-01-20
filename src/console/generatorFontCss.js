241bd9839e452be09b4f96b9fcfb511bc26ddc1d
黄学坚 <313566117@qq.com>
2017-01-20 18:58:37 +0800

    自动生成字体

---
+++ src/console/generatorFontCss.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Freax on 17-1-20.
+ * @blog http://www.myfreax.com
+ */
+const fontsConfig = require('../config/font');
+const appConfig = require('../config/app');
+const fs = require('fs');
+const path = require('path');
+
+
+let template = font => {
+    let urls = font.src.map(url => {
+        url = url.toString();
+        if (url.length > 0) {
+            return `url(${url}) format('${url.substr(parseInt(url.lastIndexOf('.')) + 1)}')`
+        }
+    }).join(',');
+    return `@font-face {font-family: ${font.name};src: ${urls};}`;
+
+};
+
+async function generate() {
+    try {
+        let content = fontsConfig.map(font => {
+            return template(font);
+        }).join('');
+        await new Promise((resolve, reject) => {
+            fs.writeFile(path.resolve(process.cwd(), appConfig.fontCssPath), content, 'utf8', (err, data) => {
+                if (err)return reject(err);
+                return resolve(true);
+            });
+        });
+        console.info('字体css文件以重新生成');
+    } catch (e) {
+        console.info(e.stack);
+    }
+}
+
+generate();
