4bb651db14378a0517b0639984e487cedfdaddd3
黄学坚 <313566117@qq.com>
2017-01-22 17:02:32 +0800

    基本实现对齐线

--- src/controller.js
+++ src/controller.js
@@ -13,25 +13,25 @@ app.controller('canvasCtrl', ['$scope', 'commonFactory', ($scope, commonFactory)
     let world = new fabric.Text('world', {
         top: 0,
         left: 0,
-        hasControls:false
+        hasControls: false
     });
 
     let hello = new fabric.Text('hello', {
-        top: 100,
-        left: 0,
-        hasControls:false
+        top: 50,
+        left: 40,
+        hasControls: false
 
     });
 
     let excl = new fabric.Text('!!', {
         top: 100,
         left: 10,
-        hasControls:false
+        hasControls: false
 
     });
 
 
-    canvas.add(world, hello,excl);
+    canvas.add(world, hello, excl);
 
 
     console.info('高度', world.getHeight());   //高度
@@ -49,6 +49,19 @@ app.controller('canvasCtrl', ['$scope', 'commonFactory', ($scope, commonFactory)
         canvas.renderAll();
     }
 
+
+    function findDuplicates(data) {
+        let result = [];
+        data.forEach(function (element, index) {
+            if (data.indexOf(element, index + 2) > -1) {
+                if (result.indexOf(element) === -1) {
+                    result.push(element);
+                }
+            }
+        });
+        return result;
+    }
+
     canvas
         .on('object:selected', updateScope)
         .on('group:selected', updateScope)
@@ -62,13 +75,58 @@ app.controller('canvasCtrl', ['$scope', 'commonFactory', ($scope, commonFactory)
         console.info('上左', options.target.getLeft(), options.target.getTop());
         let points = [];
         let ele = canvas.getObjects();
+        console.info(ele);
 
         for (let i = 0; i < ele.length; i++) {
-            points.push(ele[i].getLeft());
-            points.push(ele[i].getTop());
+            points.push(ele[i].getLeft(), ele[i].getTop());
         }
 
         console.info(points);
+        let dup = findDuplicates(points);
+        let line = null;
+
+        //查找点
+        if(dup.length > 0){
+            let firstPos = points.indexOf(dup[0]);
+            let sedPos = points.lastIndexOf(dup[0]);
+            console.info('indexOf',points[firstPos],firstPos%2===0?points[firstPos+1]:points[firstPos-1]);
+            console.info('lastIndexOf',points[sedPos],sedPos%2===0?points[sedPos+1]:points[sedPos-1]);
+
+            //  画线
+            if (firstPos%2===0){
+                // 这条线应该在全局共享，方便查找移除
+                 line = new fabric.Line([0, 0, 0, 900], {
+                    left: points[firstPos],
+                    top: 0,
+                    stroke: 'red',
+                    selectable:false
+                });
+                console.info('画x轴的偏移线',{
+                    left: points[firstPos],
+                    top: 0,
+                    stroke: 'red',
+                    selectable:false
+                })
+            }else{
+                 line = new fabric.Line([1440, 0, 0, 0], {
+                    left: 0,
+                    top: points[firstPos],
+                    stroke: 'red',
+                    selectable:false
+                });
+                console.info('画y轴的偏移线', {
+                    left: 0,
+                    top: points[firstPos],
+                    stroke: 'red',
+                    selectable:false
+                })
+            }
+            canvas.add(line);
+            //canvas.remove(line);
+
+
+        }
+
     });
 
 
